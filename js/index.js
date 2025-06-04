548feceb1d59c4e6f94c50eabcb521892b2623df
devWillian <willian.dev.tech@gmail.com>
2025-06-04 11:23:09 -0300

    fix(carousel): Resolve o problema do carrossel, a primeira barra de progresso iniciava preenchida.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let itemCurrent = 0;
+let isFirstRun = true; // Garantir um atraso(delay) na página
 let carouselContainer = document.querySelector(".carousel__container");
 let buttonElements = Array.from(document.querySelectorAll(".carousel__button"));
 
@@ -10,13 +11,22 @@ function resetProgress() {
 }
 
 function progressBar(itemCurrent) {
+  resetProgress();
   let progress = buttonElements[itemCurrent].querySelector(
     ".carousel__button__progress"
   );
-  resetProgress();
-  progress.classList.add("carousel__button__progress--active");
+
+  // Garantir um atraso(delay) na página apenas para o primeiro item do carrossel.
+  if (isFirstRun) {
+    setTimeout(() => {
+      progress.classList.add("carousel__button__progress--active");
+    }, 50);
+    isFirstRun = false;
+  } else {
+    progress.classList.add("carousel__button__progress--active");
+  }
   carouselContainer.style = `transform: translateX(-${itemCurrent * 100}%)`;
-  itemCurrent = (itemCurrent + 1) % buttonElements.length
+  itemCurrent = (itemCurrent + 1) % buttonElements.length;
   setTimeout(() => {
     progressBar(itemCurrent);
   }, 5000);
